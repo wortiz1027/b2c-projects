c3d6a0bc2d4b3f7068439ee38f30b28a8b6893e1
Brian Suarez <briansuarez@MacBook-Pro-de-avvillas.local>
2020-09-07 22:24:18 -0500

    :construction: Add template to angular

--- frontend/b2c-front/src/app/app.component.ts
+++ frontend/b2c-front/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'b2c-front';
+  title = 'argon-dashboard-angular';
 }
